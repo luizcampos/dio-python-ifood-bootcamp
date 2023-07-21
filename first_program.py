65303bed5a012ea72db6603b372b69279ee6b405
luizcampos <felipe_luiz96@yahoo.com>
2023-07-21 00:21:30 -0300

    Arquivos iniciais

---
+++ first_program.py
@@ -0,0 +1,31 @@
+print("Hello World!")
+
+print(20 + 2)
+print("Fellipe")
+print(True)
+
+### Variáveis ###
+
+name = "Fellipe"
+age = 24
+
+city, state = "Guará", "SP"
+
+print(name, age, city, state)
+
+BR_STATES =  ["SP", "MG", "RJ"]
+
+print(BR_STATES)
+
+### Conversão ###
+
+print(int(22.2))
+print(float(44))
+print(str(44))
+print(int("987"))
+
+print(100 / 2)
+print(100 // 2)
+
+print(type(int("987")))
+print(type(str(44)))
---
+++ input_output_valor.py
@@ -0,0 +1,9 @@
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+print(nome, idade)
+print(nome, idade, end="...\n")
+print(nome, idade, "anos", sep="-")
+print(nome, idade, sep="-", end="...\n")
+
+print(29, 9, 1998, sep="/")
