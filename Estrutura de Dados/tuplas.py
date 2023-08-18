1d0fe54eeed3ca9647c9232556862cd6268e4cb6
luizcampos <felipe_luiz96@yahoo.com>
2023-08-18 02:26:19 -0300

    Tuplas

---
+++ Estrutura de Dados/tuplas.py
@@ -0,0 +1,15 @@
+
+
+frutas = ("maçã", "melancia", "uva",)
+
+numbers = tuple([1, 44, 56])
+
+matrizTup = (
+    (4,"a", 5),
+    (8, 6, "b"),
+    ("c", 0, 12)
+)
+
+
+for fruta in frutas:
+    print(fruta)
