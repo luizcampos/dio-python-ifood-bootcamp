0c08ee55f93814826c6b327e115646ef535f2bc9
luizcampos <felipe_luiz96@yahoo.com>
2023-08-23 22:10:25 -0300

    Conjuntos

---
+++ Estrutura de Dados/conjuntos.py
@@ -0,0 +1,19 @@
+
+# CONJUNTOS
+
+linguagens = set(("python", "c", "java", "c+"))
+
+print(linguagens)
+
+numeros = {1, 2, 3, 4, 5}
+
+for num in numeros:
+    print(num)
+
+
+# MÉTODOS
+
+conjuntoA = {1, 2}
+conjuntoB = {3, 4}
+
+print(conjuntoA.union(conjuntoB))
