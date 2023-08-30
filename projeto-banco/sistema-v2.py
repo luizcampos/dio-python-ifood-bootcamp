70f0bd8714816e3b85abd7ae5a20a8d95d9f4427
luizcampos <felipe_luiz96@yahoo.com>
2023-08-30 00:17:58 -0300

    Versão atualizada do projeto V2

--- projeto-banco/sistema-v2.py
+++ projeto-banco/sistema-v2.py
@@ -5,6 +5,9 @@ extrato = ""
 usuarios = {
     1: {"nome":"Luiz Fellipe", "dataNascimento": "29/09/1999", "cpf": "25874136988", "endereco": "João Santos, 287 - Pelourinho - Lorena/SP"}
 }
+contas = {
+    1: {"agencia": "0001", "usuario": "25874136988", "conta": 1}
+}
 menu = f"""================================
 =             MENU             =
 ================================
@@ -14,6 +17,8 @@ menu = f"""================================
 =   4 - Cadastrar usuário      =
 =   5 - Criar conta corrente   =
 =   6 - Listar usuários        =
+=   7 - Listar contas          =
+=   8 - Excluir usuário        =
 =   0 - Sair                   = 
 ================================
 Digite um número: """  
@@ -54,18 +59,39 @@ def gerar_extrato(*, extrato):
 
 def criar_usuario():
     global usuarios
+    sucesso, cpf_existe = False, False
     qtde = len(usuarios)
 
     nome = str(input("Digite o nome: "))
     data = str(input("Digite a data de nascimento (ex: 10/05/2001): "))
-    cpf = str(input("Digite o CPF: "))
+    
+    while sucesso == False:
+        aux = False
+        cpf = input("Digite o CPF (apenas números): ")
 
-    if int(cpf) > 0:
-        print("cpf válido")
-    else:
-        print("cpf inválido")
+        if cpf != "":
+            if  len(cpf) == 11:
+                #verificar se cpf existe
+                for chave, valor in usuarios.items():
+                    if usuarios[chave]["cpf"] == str(cpf):
+                        print("CPF já existe! Tente novamente")
+                        aux = True
+                        break
+                    else:
+                        aux = False
+                if aux == False:
+                    try:
+                        int(cpf) #só numeros?
+                    except ValueError as e:
+                        print("CPF inválido!")
+                    else:
+                        sucesso = True  #é só num, converteu pra int
+                        break
+            else:
+                print("CPF inválido! Digite um CPF com 11 caracteres")
 
-    #verificar se cpf existe
+        else:
+            print("CPF inválido!")
 
     rua = str(input("Digite o nome rua: "))
     num = str(input("Digite o nº da casa: "))
@@ -79,17 +105,102 @@ def criar_usuario():
     usuarios[qtde+1]["data"] = data
     usuarios[qtde+1]["cpf"] = cpf
     usuarios[qtde+1]["endereco"] = endereco
-
-    print(usuarios)
     
 
-#def criar_conta_corrente():
+def criar_conta_corrente():
+    global contas, usuarios
+    qtde = len(contas)
+    sucesso = False
 
+    contas.setdefault(qtde+1, {})
+    contas[qtde+1]["agencia"] = "0001"
+
+    while sucesso == False:
+        user = str(input("Digite o CPF do cliente: "))
+
+        if user != "":
+            #verificar se usuário existe
+            for chave, valor in usuarios.items():
+                if usuarios[chave]["cpf"] == user:
+                    try:
+                        int(user) #só numeros?
+                    except ValueError as e:
+                        print("Cliente inválido!")
+                    else:
+                        contas[qtde+1]["usuario"] = user
+                        sucesso = True  #é só num, converteu pra int
+                        break
+
+            if sucesso == False:
+                print("Usuário não encontrado!")
+        else:
+            print("Usuário inválido!")
+    
+    contas[qtde+1]["conta"] = int(contas[qtde]["conta"])+1
+
+    #sucesso = False
+    #contaExist = True
+
+    # while sucesso == False:
+    #     conta = str(input("Digite o número da conta: "))
+
+    #     if conta != "":
+    #         #verificar se conta existe
+    #         for chave, valor in usuarios.items():
+    #             print(chave, contas[chave]["conta"], conta)
+    #             if contas[chave]["conta"] == conta:
+    #                     print("Conta já existe! Tente novamente")
+    #                     break
+    #             else:
+    #                 contaExist = False
+            
+    #         if contaExist == False:
+    #             try:
+    #                 int(conta) #só numeros?
+    #             except ValueError as e:
+    #                 print("Número da conta inválida!")
+    #             else:
+    #                 contas[qtde+1]["conta"] = conta
+    #                 sucesso = True  #é só num, converteu pra int
+    #                 break
+    #     else:
+    #         print("Número da conta inválida!")
+
+def excluir_cliente():
+
+    global usuarios
+    sucesso = False
+
+    while sucesso == False:
+        cpf = str(input("Digite o CPF do usuário: "))
+        if cpf != "":
+                #verificar se usuário existe
+                for chave, valor in usuarios.items():
+                    if usuarios[chave]["cpf"] == cpf:
+                        try:
+                            int(cpf) #só numeros?
+                        except ValueError as e:
+                            print("Usuário inválido!")
+                        else:
+                            del usuarios[chave]
+                            sucesso = True
+                            print("Usuário excluído com sucesso!")
+                            break
+        else:
+            print("CPF inválido! Tente novamente")
 
 def listar_usuarios():
     global usuarios
 
+    print("Sem usuários cadastrados" if not usuarios else "")
+
     for chave, valor in usuarios.items():
+        print(chave, valor)
+
+def listar_contas():
+    global contas
+
+    for chave, valor in contas.items():
         print(chave, valor)
 
 while True:
@@ -112,9 +223,18 @@ while True:
 
     elif opcao == "4":
         criar_usuario()
+    
+    elif opcao == "5":
+        criar_conta_corrente()
 
     elif opcao == "6":
         listar_usuarios()
+
+    elif opcao == "7":
+        listar_contas()
+
+    elif opcao == "8":
+        excluir_cliente()
         
     elif opcao == "0":
         print("Sistema encerrado. Obrigado!")
